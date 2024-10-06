3893139a1312acdb72574f6e593fd7e5bdc54808
Mithila001 <136560946+Mithila001@users.noreply.github.com>
2024-10-06 19:08:32 +0530

    add two connection strings again

---
+++ ConnectionString.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PharmacyManagementSystem._Common
+{
+    public static class CommonConnecString
+    {
+        public static string ConnectionString
+        {
+            get
+            {
+                return @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=E:\Programming\PMS\PharmacyManagementSystem\_Common\Database_M.mdf;Integrated Security=True";
+            }
+        }
+    }
+
+    
+}
