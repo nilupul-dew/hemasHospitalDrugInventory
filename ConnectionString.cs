54ae3738930a122dc436874202b88e3412c8ea3e
Mithila001 <136560946+Mithila001@users.noreply.github.com>
2024-10-06 18:56:37 +0530

    2 connection string s

--- ConnectionString.cs
+++ ConnectionString.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace PharmacyManagementSystem._Common
 {
-    public static class CommonConnecString
+    public static class CommonConnecStringM
+    {
+        public static string ConnectionString
+        {
+            get
+            {
+                return @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=E:\Programming\PMS\PharmacyManagementSystem\_Common\Database_M.mdf;Integrated Security=True";
+            }
+        }
+    }
+
+    public static class CommonConnecStringD
     {
         public static string ConnectionString
         {
