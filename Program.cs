f0ef9e9f7f760f34d51d0b23e0ff2e946dda5cbc
dananjaya2002 <kavishandananjaya2002@gmail.com>
2024-10-06 15:04:58 +0530

    ch

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace hemasHospitalDrugInventory
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new BarChart());
         }
     }
 }
