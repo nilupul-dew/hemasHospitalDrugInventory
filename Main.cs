9278f5bfe29a84df9ee4d66820ad6601d532c9ee
dananjaya2002 <kavishandananjaya2002@gmail.com>
2024-10-07 22:32:18 +0530

    cg

--- Main.cs
+++ Main.cs
@@ -17,6 +17,10 @@ namespace hemasHospitalDrugInventory.Suppliers
 {
     public partial class Main : Form
     {
+        // Declare user controls as class-level variables
+        S_UC_Index s_UC_Index; // Employee user control
+        I_UC_Main i_UC_Main; // Inventory user control
+        T_UC_Main t_UC_Main; // Transaction user control
         public Main()
         {
             InitializeComponent();
@@ -25,23 +29,29 @@ namespace hemasHospitalDrugInventory.Suppliers
 
         void InitializeUserControls()
         {
+
             // Initialize user controls
             T_UC_Main t_UC_Main = new T_UC_Main();
             S_UC_Index s_UC_Index = new S_UC_Index();
             I_UC_Main i_UC_Main = new I_UC_Main();
-            UC_Home uc_Home = new UC_Home();
+            
+
+            t_UC_Main = new T_UC_Main();
+            s_UC_Index = new S_UC_Index();
+            i_UC_Main = new I_UC_Main();
+
 
             // Set the DockStyle to fill so it occupies the entire panel
             t_UC_Main.Dock = DockStyle.Fill;
             s_UC_Index.Dock = DockStyle.Fill;
             i_UC_Main.Dock = DockStyle.Fill;
-            uc_Home.Dock = DockStyle.Fill;
+ 
 
             // Add user controls to the main panel (panelMain)
             panel2.Controls.Add(t_UC_Main);
             panel2.Controls.Add(s_UC_Index);
             panel2.Controls.Add(i_UC_Main);
-            panel2.Controls.Add(uc_Home);
+
 
             // Show the first user control initially
             i_UC_Main.BringToFront();
@@ -49,13 +59,15 @@ namespace hemasHospitalDrugInventory.Suppliers
 
         private void employee_btn_Click(object sender, EventArgs e)
         {
-           
-
+            s_UC_Index.BringToFront();
         }
-
         private void transaction_btn_Click(object sender, EventArgs e)
         {
-
+            t_UC_Main.BringToFront();
+        }
+        private void Inventory_btn_Click(object sender, EventArgs e)
+        {
+            i_UC_Main.BringToFront();
         }
     }
 }
