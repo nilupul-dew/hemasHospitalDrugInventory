8305520a320a1d3a857c76ef9b78845234c183f9
dananjaya2002 <kavishandananjaya2002@gmail.com>
2024-10-07 20:24:49 +0530

    ch

---
+++ ConnectString.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hemasHospitalDrugInventory
+{
+    internal class ConnectString
+    {
+        public static string ConnectionString
+        {
+            get
+            {
+                return @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\User\source\repos\hemasHospitalDrugInventory\Database_SIM.mdf;Integrated Security=True";
+            }
+        }
+    }
+}
