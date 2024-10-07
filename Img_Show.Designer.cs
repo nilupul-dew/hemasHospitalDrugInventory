1762c80f3199e74ad8be6ef714d98e32f8e9ab7e
dananjaya2002 <kavishandananjaya2002@gmail.com>
2024-10-07 14:47:25 +0530

    ch

--- Img_Show.Designer.cs
+++ Img_Show.Designer.cs
@@ -30,39 +30,76 @@
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.Load_btn = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.select_btn = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(292, 57);
+            this.pictureBox1.Location = new System.Drawing.Point(26, 48);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(346, 209);
+            this.pictureBox1.Size = new System.Drawing.Size(179, 119);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
             // Load_btn
             // 
             this.Load_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Load_btn.Location = new System.Drawing.Point(392, 306);
+            this.Load_btn.Location = new System.Drawing.Point(73, 210);
             this.Load_btn.Name = "Load_btn";
-            this.Load_btn.Size = new System.Drawing.Size(133, 46);
+            this.Load_btn.Size = new System.Drawing.Size(79, 46);
             this.Load_btn.TabIndex = 2;
             this.Load_btn.Text = "Load";
             this.Load_btn.UseVisualStyleBackColor = true;
             this.Load_btn.Click += new System.EventHandler(this.Load_btn_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(256, 48);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(521, 225);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEndEdit);
+            // 
+            // select_btn
+            // 
+            this.select_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.select_btn.Location = new System.Drawing.Point(423, 297);
+            this.select_btn.Name = "select_btn";
+            this.select_btn.Size = new System.Drawing.Size(79, 46);
+            this.select_btn.TabIndex = 4;
+            this.select_btn.Text = "select";
+            this.select_btn.UseVisualStyleBackColor = true;
+            this.select_btn.Click += new System.EventHandler(this.select_btn_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(610, 297);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(143, 108);
+            this.listBox1.TabIndex = 5;
             // 
             // Img_Show
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.select_btn);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.Load_btn);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Img_Show";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Img_Show";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -70,5 +107,8 @@
         #endregion
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button Load_btn;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button select_btn;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
--- Img_Show.cs
+++ Img_Show.cs
@@ -19,6 +19,7 @@ namespace hemasHospitalDrugInventory
         public Img_Show()
         {
             InitializeComponent();
+            LoadData();
         }
 
         private void Load_btn_Click(object sender, EventArgs e)
@@ -45,6 +46,103 @@ namespace hemasHospitalDrugInventory
                 {
                     MessageBox.Show("No image found for the given ID.");
                 }
+            }
+        }
+
+        private void LoadData()
+        {
+            using (SqlConnection conn = new SqlConnection(CommonConnecString.ConnectionString))
+            {
+                try
+                {
+                    conn.Open();
+
+                    // SQL query to select data from the table
+                    string query = "SELECT ID, Name, Age FROM Person";
+
+                    SqlDataAdapter dataAdapter = new SqlDataAdapter(query, conn);
+                    DataTable dataTable = new DataTable();
+                    dataAdapter.Fill(dataTable);
+
+                    // Add a checkbox column (unbound column)
+                    DataGridViewCheckBoxColumn checkBoxColumn = new DataGridViewCheckBoxColumn
+                    {
+                        HeaderText = "Select",
+                        Width = 50,
+                        Name = "checkBoxColumn"
+                    };
+
+                    // Add the checkbox column to DataGridView
+                    dataGridView1.Columns.Add(checkBoxColumn);
+
+                    // Bind the database data to DataGridView
+                    dataGridView1.DataSource = dataTable;
+
+                    // Set the DataGridView properties
+                    dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+        }
+
+        private void select_btn_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear(); // Clear previous items
+
+            List<int> selectedPrimaryKeys = new List<int>();
+
+            // Loop through the DataGridView rows
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                // Check if the checkbox is checked
+                if (Convert.ToBoolean(row.Cells["checkBoxColumn"].Value))
+                {
+                    // Add the primary key (ID) to the list
+                    int primaryKey = Convert.ToInt32(row.Cells["ID"].Value);
+                    selectedPrimaryKeys.Add(primaryKey);
+
+                    // Add the primary key to the ListBox
+                    listBox1.Items.Add(primaryKey);
+                }
+            }
+
+            if (selectedPrimaryKeys.Count > 0)
+            {
+                MessageBox.Show("Selected Primary Keys added to ListBox.");
+            }
+            else
+            {
+                MessageBox.Show("No rows selected.");
+            }
+        }
+
+        private void dataGridView1_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == dataGridView1.Columns["Age"].Index) // Check if it's the Age column
+            {
+                // Get the new Age value and the corresponding ID
+                int id = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells["ID"].Value);
+                int newAge = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells["Age"].Value);
+
+                // Update the Age in the database
+                UpdateAgeInDatabase(id, newAge);
+            }
+        }
+
+        // Method to update Age in the database
+        private void UpdateAgeInDatabase(int id, int newAge)
+        {
+            using (SqlConnection conn = new SqlConnection(CommonConnecString.ConnectionString))
+            {
+                conn.Open();
+                string query = "UPDATE Person SET Age = @newAge WHERE ID = @id";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@newAge", newAge);
+                cmd.Parameters.AddWithValue("@id", id);
+                cmd.ExecuteNonQuery();
             }
         }
     }
