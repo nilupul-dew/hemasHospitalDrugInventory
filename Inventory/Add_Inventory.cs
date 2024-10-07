efbf5b55dd4e7065b18e690e91e43784b401877a
dananjaya2002 <kavishandananjaya2002@gmail.com>
2024-10-07 16:17:30 +0530

    ch

---
+++ Inventory/Add_Inventory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hemasHospitalDrugInventory.Inventory
+{
+    public partial class Add_Inventory : Form
+    {
+        public Add_Inventory()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Inventory/Update_Inventory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hemasHospitalDrugInventory.Inventory
+{
+    public partial class Update_Inventory : Form
+    {
+        public Update_Inventory()
+        {
+            InitializeComponent();
+        }
+    }
+}
